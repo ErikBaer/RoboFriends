a84c5428bbc459420c9b359c839fb66c4b68735a
Erik Baer <erik.baer@yahoo.de>
2020-02-14 00:07:44 +0100

    create cardArray to generate Cards

--- .history/src/CardList_20200214000707.js
+++ .history/src/CardList_20200214000707.js
@@ -3,13 +3,13 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
 
-    const cardsArray = robots.map((user,i) => {
+    const cardArray = robots.map((user,i) => {
         return <Card id={robots[i].id} name ={robots[i].name} email={robots[i].email} />
     })
 
     return (
         <div>
-            {cardsArray}
+            {cardArray}
         </div> 
     );
 }
