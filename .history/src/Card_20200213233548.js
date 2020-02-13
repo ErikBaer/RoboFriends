86572c0a5249cee305439671ca66bd314755b528
Erik Baer <erik.baer@yahoo.de>
2020-02-13 23:36:33 +0100

    add props to Card

--- .history/src/Card_20200213233548.js
+++ .history/src/Card_20200213233548.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = () => {
    return (
     <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
         <img src='https://robohash.org/test?200x200' alt='robot'/>
--- .history/src/index_20200213232828.js
+++ .history/src/index_20200213232828.js
@@ -5,13 +5,13 @@ import './index.css';
 import Card from './Card.js'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import {robots} from './robots';
+import robots from './robots.js'
 
 ReactDOM.render(
                 <div>
-                    <Card id={robots[0].id} name ={robots[0].name} email={robots[0].email} />
-                    <Card id={robots[1].id} name ={robots[1].name} email={robots[1].email}/>
-                    <Card id={robots[2].id} name ={robots[2].name} email={robots[2].email}/>
+                    <Card />
+                    <Card />
+                    <Card />
                 </div>    
 , document.getElementById('root'));
 
