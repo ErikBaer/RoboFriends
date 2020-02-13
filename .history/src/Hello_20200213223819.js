f7c46a0c97b43bea1a210deaa9b591638df6dde2
Erik Baer <erik.baer@yahoo.de>
2020-02-13 22:38:45 +0100

    create Hello World Component

---
+++ .history/src/Hello_20200213223819.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+
+class Hello extends Component {
+    render() {
+        return <h1>Hello World</h1>
+    }
+}
+
+export default Hello
