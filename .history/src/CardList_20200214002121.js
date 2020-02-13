9d11d861d1ee9d6f5d453aea59ac05be4541711f
Erik Baer <erik.baer@yahoo.de>
2020-02-14 00:22:33 +0100

    implement cardsArray-Code straight into Fragment

--- .history/src/CardList_20200214002121.js
+++ .history/src/CardList_20200214002121.js
@@ -3,6 +3,17 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
 
+    const cardsArray = robots.map((user,i) => {
+        return (
+        <Card 
+        key={robots[0].id} 
+        id={robots[i].id} 
+        name ={robots[i].name} 
+        email={robots[i].email} 
+        />
+        )
+    })
+
     return (
         <Fragment>
             {
