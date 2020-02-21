30639270ca1afb4a9b8e5735b5acfaabbcee1653
Erik Baer <erik.baer@yahoo.de>
2020-02-22 00:12:36 +0100

    create App.js and move cardlist

---
+++ .history/src/App_20200222000922.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Cardlist from './CardList'
+
+
+const App = () =>{
+    return(
+        <CardList robots={robots}/>
+    )
+}
---
+++ .history/src/App_20200222000944.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CardList from './CardList';
+import
+
+
+const App = () =>{
+    return(
+        <CardList robots={robots}/>
+    )
+}
