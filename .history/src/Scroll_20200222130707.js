687d27b8531b8d2a08360791cd40248cb0e736de
Erik Baer <erik.baer@yahoo.de>
2020-02-22 13:41:31 +0100

    make cardList scrollable

--- .history/src/Scroll_20200222130707.js
+++ .history/src/Scroll_20200222130707.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{overflowY: 'scroll', border: '3px solid black', height: '800px'}}>
+        <div style={{overflowY: 'scroll', border: '3px solid black', height: '1000px'}}>
             {props.children}
         </div>
     );
-};
+}
 
 export default Scroll;
